62e85d02f9629eb16f5795315579eba5a1197f7e
Bhupesh Pravin Cholake <76155786+RealBhupesh@users.noreply.github.com>
2025-09-17 17:37:31 +0530

    Update layout.tsx
    changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Anushka Shirsath App',
+  description: 'made by me ',
 }
 
 export default function RootLayout({
